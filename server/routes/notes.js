3c854b23e37b9fa1030f2c0a08a25191fb269d4a
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-03 15:00:54 +0200

    Create GET route for all notes

---
+++ server/routes/notes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const NoteModel = require("../models/NoteModel");
+
+//GET ALL NOTES
+router.get("/", async (req, res) => {
+	try {
+		const notes = await NoteModel.find();
+		if (notes.length === 0 || !notes) {
+			return res.status(404).json("Error: Notes not found!");
+		}
+		return res.status(200).json(notes);
+	} catch (err) {
+		return res.status(400).json({ error: err });
+	}
+});
+
+module.exports = router;
