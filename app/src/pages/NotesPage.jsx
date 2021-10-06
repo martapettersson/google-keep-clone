240cc560c95eb661d488366ae63ccd39212c7ce4
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-06 15:20:26 +0200

    Add rendering logic in NotesPage

--- app/src/pages/NotesPage.jsx
+++ app/src/pages/NotesPage.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import Note from "../components/Note";
 import CreateNote from "../components/CreateNote";
+import EditNote from "../components/EditNote";
 
-export default function NotesPage() {
+export default function NotesPage(props) {
 	const {
 		notes, setNotes , getNotes
 	} = useContext(UserContext);
@@ -12,19 +14,49 @@ export default function NotesPage() {
 		getNotes();
 	}, []);
 	
-	return (
-		<div>
-			<h1>Notes</h1>
-			<CreateNote notes= {notes} setNotes = {setNotes}/>
-			{notes ? (
+	if(props.match.params.id && notes) {
+		const id = props.match.params.id;
+		const note = notes.find(note => note._id === id);
+		if (note) {
+			return (
 				<div>
-					{notes.map((note) => {
-						return <Note key={note.id} note={note} notes= {notes} setNotes = {setNotes} />;
-					})}
+					<h1>Notes Edit</h1>
+					<EditNote id={id} note={note} notes= {notes} setNotes = {setNotes}/>
+					{notes ? (
+						<div>
+							{notes.map((note) => {
+								return <Note key={note._id} note={note} notes= {notes} setNotes = {setNotes} />;
+							})}
+						</div>
+					) : (
+						<p>Loading Data</p>
+					)}
 				</div>
-			) : (
-				<p>Loading Data</p>
-			)}
-		</div>
-	);
+			);
+		} else {
+			return (
+				<div>
+					<h1>Page Not Found!</h1>
+					<Link to={`/notes`}>Back To Notes</Link>
+				</div>
+			)
+		}
+	} else {
+		console.log(notes)
+		return (
+			<div>
+				<h1>Notes</h1>
+				<CreateNote notes= {notes} setNotes = {setNotes}/>
+				{notes ? (
+					<div>
+						{notes.map((note) => {
+							return <Note key={note._id} note={note} notes= {notes} setNotes = {setNotes} />;
+						})}
+					</div>
+				) : (
+					<p>Loading Data</p>
+				)}
+			</div>
+		);
+	}
 }
