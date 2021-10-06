4dcc72a0102342bac027ec5f68f5d4bd8ff58d90
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-06 12:06:34 +0200

    Handle change in input fields and set to formData

--- app/src/components/CreateNote.jsx
+++ app/src/components/CreateNote.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateNote({ notes, setNotes }) {
+    const [formData, setFormData] = useState({});
+
+    const handleOnChange = (e) => {
+		const inputName = e.target.name;
+		const inputValue = e.target.value;
+		setFormData({ ...formData, [inputName]: inputValue });
+        console.log(formData);
+	};
 
 	return (
         <div>
             <form action="post">
                 <h3>Title</h3>
-                <input type="text" name="title" id="title" value="Title" />
+                <input onChange={handleOnChange} type="text" name="title" id="title" value={formData["title"] || ""} />
                 <br />
                 <h3>Body</h3>
-                <textarea name="body" id="body" cols="30" rows="10" value="Body"></textarea>
+                <textarea onChange={handleOnChange} name="body" id="body" cols="30" rows="10" value={formData["body"] || ""}></textarea>
                 <br />
                 <input type="submit" value="Create" />
             </form>
