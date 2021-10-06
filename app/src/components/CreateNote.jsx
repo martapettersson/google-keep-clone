c390dd06c754b7c55fbefdef05c980c76015fe9e
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-06 12:12:31 +0200

    Add validate form function

--- app/src/components/CreateNote.jsx
+++ app/src/components/CreateNote.jsx
@@ -7,23 +7,34 @@ export default function CreateNote({ notes, setNotes }) {
 		const inputName = e.target.name;
 		const inputValue = e.target.value;
 		setFormData({ ...formData, [inputName]: inputValue });
-        console.log(formData);
 	};
+
+    const validateForm = () => {
+        if (!formData.title || !formData.body){
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     const createNote = (e) => {
         e.preventDefault();
-        const url = `http://localhost:5000/api/notes/`;
-        fetch(url, {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }).then((res) => res.json())
-        .then((data) => {
-            setNotes([...notes, data.data]);
-            setFormData({});
-        });
+        if (validateForm() === true) {
+            const url = `http://localhost:5000/api/notes/`;
+            fetch(url, {
+                method: "POST",
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((res) => res.json())
+            .then((data) => {
+                setNotes([...notes, data.data]);
+                setFormData({});
+            });
+        } else {
+            alert ("Please enter a title and body.");
+        }
     }
 
 	return (
