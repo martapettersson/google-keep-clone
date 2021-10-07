f37c0de5dd2859b92fb06f16027e346025e301f7
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-07 13:27:40 +0200

    Install dompurify, jsdom and marked and update note schema to sanitize user input

--- server/models/NoteModel.js
+++ server/models/NoteModel.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
+const marked = require("marked");
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
 
 const NoteSchema = new mongoose.Schema(
 	{
-		title: { type: String, required: true },
-		body: { type: String, required: true },
+		markdown: { type: String, required: true },
+		sanitizedHtml: { type: String, required: true },
 	},
 	{ timestamps: true }
 );
+
+NoteSchema.pre("validate", function () {
+	if (this.markdown) {
+		this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
+	}
+});
 
 module.exports = mongoose.model("Note", NoteSchema);
