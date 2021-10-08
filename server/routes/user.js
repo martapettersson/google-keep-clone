862a6fba4b85c1dc29e2ca81549239c47e2d0775
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-08 17:51:04 +0200

    Add routes for get, create and login hard coded users with hashed passwords

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,38 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.post("/login", (req, res) => {
-	res.json("login");
+const users = [];
+
+router.get("/", (req, res) => {
+	res.json(users);
+});
+
+router.post("/", async (req, res) => {
+	const password = req.body.password;
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = { name: req.body.name, password: hashedPassword };
+		users.push(user);
+		res.json(users);
+	} catch (err) {
+		return res.status(400).json({ success: false, error: err });
+	}
+});
+
+router.post("/login", async (req, res) => {
+	const user = users.find((user) => user.name === req.body.name);
+	if (!user) {
+		return res.status(400).json({ success: false, error: "Cannot find user" });
+	}
+	try {
+		if (await bcrypt.compare(req.body.password, user.password)) {
+			res.send("Success");
+		} else {
+			res.send("Not allowed");
+		}
+	} catch (err) {
+		return res.status(400).json({ success: false, error: err });
+	}
 });
 
 module.exports = router;
