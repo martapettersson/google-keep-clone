e80dcd5e6efabc352685222be07bc5a0f9593411
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-16 14:07:45 +0200

    Add protect middleware with token verification

---
+++ server/routes/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+// Verify token
+module.exports = function (req, res, next) {
+	const token = req.header("auth-token");
+	if (!token)
+		return res.status(401).json({ success: false, error: "Access denied" });
+	try {
+		const verified = jwt.verify(token, process.env.SECRET_TOKEN);
+		req.user = verified;
+		next();
+	} catch (err) {
+		res.status(400).json({ success: false, error: "Invalid token" });
+	}
+};
