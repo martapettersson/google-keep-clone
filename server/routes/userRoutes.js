f605622f888a8842ae836595003c80ec3a28918d
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-25 15:46:44 +0200

    Update login, signup, generateToken andadd getMe

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,17 +2,27 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/UserModel");
+const verify = require("./verifyToken");
 
 const secretToken = process.env.SECRET_TOKEN;
 const salt = parseInt(process.env.SALT);
 
 const generateToken = (userId) => {
-	return jwt.sign({ data: userId }, secretToken, { expiresIn: "60m" });
+	return jwt.sign({ id: userId }, secretToken, { expiresIn: "60m" });
 };
 
 //GET ALL USERS
+// ONLY DEVELOPMENT
 router.get("/", (req, res) => {
 	User.find()
+		.then((users) => res.status(200).json({ success: true, data: users }))
+		.catch((err) => res.status(400).json({ success: false, error: err }));
+});
+
+//GET SPECIFIC USER
+// ONLY DEVELOPMENT
+router.get("/getMe", verify, async (req, res) => {
+	User.findOne({ _id: req.user.id })
 		.then((users) => res.status(200).json({ success: true, data: users }))
 		.catch((err) => res.status(400).json({ success: false, error: err }));
 });
@@ -38,7 +48,10 @@ router.post("/signup", async (req, res) => {
 		newUser
 			.save()
 			.then((user) => {
-				res.status(201).json({ token: generateToken(user._id) });
+				console.log(user);
+				const token = generateToken(user._id);
+				console.log(token);
+				res.status(201).json({ success: true, token });
 			})
 			.catch((err) => {
 				res.status(400).json({ success: false, error: err });
@@ -59,10 +72,7 @@ router.post("/login", async (req, res) => {
 			if (err) return res.status(500).json({ success: false, error: err });
 			else if (match) {
 				const token = generateToken(user._id);
-				return res
-					.status(200)
-					.header("auth-token", token)
-					.json({ success: true, token });
+				return res.status(200).json({ success: true, token });
 			} else
 				return res
 					.status(403)
@@ -74,6 +84,7 @@ router.post("/login", async (req, res) => {
 });
 
 //DELETE USER
+// ONLY DEVELOPMENT
 router.delete("/:id", async (req, res) => {
 	const id = req.params.id;
 	try {
