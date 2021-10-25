94593b0d965a0b7656f8d96e06b385203ab244d0
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-25 14:04:12 +0200

    Add origin cors in server and clean up

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 var express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const notesRouter = require("./routes/noteRoutes");
+const usersRouter = require("./routes/userRoutes");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+	cors({
+		origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	})
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -20,11 +27,8 @@ connection.on("error", (err) => {
 	console.log(`Error connecting to MongoDB: ${err}`);
 });
 
-const notesRouter = require("./routes/notes");
 app.use("/api/notes", notesRouter);
-
-const userRouter = require("./routes/user");
-app.use("/api/user", userRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
