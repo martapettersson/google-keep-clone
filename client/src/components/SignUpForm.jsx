91345f07339d533116cd71a0237a1fee7241cd73
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-25 21:44:36 +0200

    Add sign up functionality

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function SignUpForm() {
 	const [formFields, setFormFields] = useState(null);
+
+	const history = useHistory();
 
 	const handleChange = (value, fieldId) => {
 		const payload = { ...formFields };
 		payload[fieldId] = value;
 		setFormFields(payload);
 	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const url = "/api/users/signup";
+		const payload = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formFields),
+		};
+		const response = await fetch(url, payload);
+		if (!response.ok) {
+			throw new Error("Something went wrong!");
+		}
+		const responseData = await response.json();
+		localStorage.setItem("tkn", responseData.token);
+		history.push(`/notes`);
+	};
+
 	return (
 		<div>
 			<h2>Sign Up</h2>
-			<form className="form">
+			<form onSubmit={handleSubmit} className="form">
 				<label htmlFor="fullName">Full Name</label>
 				<input
 					onChange={(e) => handleChange(e.target.value, e.target.id)}
