570c4ff0b961784ccea224fd499a1216ef93357e
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-25 14:58:38 +0200

    Add user ref to note model

--- server/models/NoteModel.js
+++ server/models/NoteModel.js
@@ -6,6 +6,11 @@ const dompurify = createDomPurify(new JSDOM().window);
 
 const NoteSchema = new mongoose.Schema(
 	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: [true, "A note must have an id of user."],
+		},
 		markdown: { type: String, required: true },
 		sanitizedHtml: { type: String, required: true },
 	},
