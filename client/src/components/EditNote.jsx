6f15e6a000493d52f1551e92336ea873a742e321
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-26 13:07:13 +0200

    Let users edit their private notes

--- client/src/components/EditNote.jsx
+++ client/src/components/EditNote.jsx
@@ -1,53 +1,58 @@
 import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
-import MDEditor from '@uiw/react-md-editor';
+import MDEditor from "@uiw/react-md-editor";
 
 export default function EditNote({ id, note, notes, setNotes }) {
-    const [formData, setFormData] = useState(note.markdown);
-    const history = useHistory();
-    const noteId = id;
+	const [formData, setFormData] = useState(note.markdown);
+	const history = useHistory();
+	const noteId = id;
 
-    const validateForm = () => {
-        if (!formData){
-            return false;
-        } else {
-            return true;
-        }
-    }
+	const validateForm = () => {
+		if (formData === note.markdown) {
+			return false;
+		} else {
+			return true;
+		}
+	};
 
-    const updateNote = (e) => {
-        e.preventDefault();
-        if (validateForm() === true) {
-            const url = `http://localhost:5000/api/notes/${noteId}`;
-            fetch(url, {
-                method: "PUT",
-                body: JSON.stringify({markdown: formData}),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then((res) => res.json())
-            .then((data) => {
-                const newNotes = notes.filter(
-                    (note) => note._id !== noteId
-                );
-                newNotes.push(data.data);
-                setNotes(newNotes)
-                history.push(`/notes`);
-            });
-        } else {
-            alert ("Please enter data in form.");
-        }
-    }
+	const updateNote = async (e) => {
+		e.preventDefault();
+		if (!validateForm()) {
+			return alert("Please enter new data in form or cancel.");
+		}
+		const url = `/api/notes/${noteId}`;
+		const token = localStorage.getItem("tkn");
+
+		const payload = {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({ markdown: formData }),
+		};
+		const response = await fetch(url, payload);
+		if (!response.ok) {
+			throw new Error("Something went wrong!");
+		}
+		const responseData = await response.json();
+		const newNotes = notes.filter((note) => note._id !== noteId);
+		newNotes.push(responseData.data);
+		setNotes(newNotes);
+		history.push(`/notes`);
+	};
 
 	return (
-        <div className="md-editor">
-            <h2 className="header">Edit Note</h2>
-            <MDEditor value={formData} onChange={setFormData} />
-            <form onSubmit={updateNote} action="post">
-                <input type="hidden" name="markdown" id="markdown" value={formData} />
-                <input className="btn" type="submit" value="Update"/>
-                <Link to="/notes" className="btn">Cancel</Link>
-            </form>
-        </div>
+		<div className="md-editor">
+			<h2 className="header">Edit Note</h2>
+			<MDEditor value={formData} onChange={setFormData} />
+			<form onSubmit={updateNote} action="post">
+				<input type="hidden" name="markdown" id="markdown" value={formData} />
+				<input className="btn" type="submit" value="Update" />
+				<Link to="/notes" className="btn">
+					Cancel
+				</Link>
+			</form>
+		</div>
 	);
 }
