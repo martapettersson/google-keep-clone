bfa47a0b13dbb8948594ef6d482f887e51f77b9e
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-26 15:03:41 +0200

    Refactor: noteRoutes functions to noteControllers

---
+++ server/controllers/noteControllers.js
@@ -0,0 +1,102 @@
+const NoteModel = require("../models/NoteModel");
+
+exports.addNote = async (req, res) => {
+  const note = {
+    userId: req.user.id,
+    markdown: req.body.markdown,
+  };
+  if (note.markdown === "" || typeof note.markdown !== "string") {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide note object with a string!",
+    });
+  }
+  const newNote = new NoteModel(note);
+  try {
+    await newNote.save();
+    return res.status(200).json({
+      success: true,
+      statusMessage: "New note added succesfully!",
+      data: newNote,
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+};
+
+exports.getAllNotes = async (req, res) => {
+  try {
+    const notes = await NoteModel.find({ userId: req.user.id });
+    if (notes.length === 0 || !notes) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Notes Not Found!" });
+    }
+    return res.status(200).json({ success: true, data: notes });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
+
+exports.getSingleNote = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await NoteModel.findOne({
+      $and: [{ _id: id }, { userId: req.user.id }],
+    });
+    if (!note) {
+      return res.status(404).json({ success: false, error: "Note Not Found!" });
+    }
+    return res.status(200).json({ success: true, data: note });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
+
+exports.updateNote = async (req, res) => {
+  const { id } = req.params;
+  const userInput = {
+    markdown: req.body.markdown,
+  };
+
+  if (userInput.markdown === "" || typeof userInput.markdown !== "string") {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide note object with a markdown string!",
+    });
+  }
+  try {
+    const note = await NoteModel.findOne({
+      $and: [{ _id: id }, { userId: req.user.id }],
+    });
+    note.markdown = userInput.markdown;
+    const updatedNote = await note.save({ new: true });
+    if (!updatedNote) {
+      return res.status(404).json({ success: false, error: "Note Not Found!" });
+    }
+    return res.status(200).json({
+      success: true,
+      statusMessage: "Note updated succesfully!",
+      data: updatedNote,
+    });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
+
+exports.deleteNote = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await NoteModel.findOneAndDelete({
+      $and: [{ _id: id }, { userId: req.user.id }],
+    });
+    if (!note) {
+      return res.status(404).json({ success: false, error: "Note Not Found!" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, statusMessage: "Note deleted succesfully!" });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+};
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,111 +1,26 @@
 const router = require("express").Router();
-const NoteModel = require("../models/NoteModel");
-const verify = require("./verifyToken");
+const verify = require("../utils/verifyToken");
+const {
+  addNote,
+  getAllNotes,
+  getSingleNote,
+  updateNote,
+  deleteNote,
+} = require("../controllers/noteControllers");
 
-//GET ALL NOTES
-router.get("/", verify, async (req, res) => {
-  try {
-    const notes = await NoteModel.find({ userId: req.user.id });
-    if (notes.length === 0 || !notes) {
-      return res
-        .status(404)
-        .json({ success: false, error: "Notes Not Found!" });
-    }
-    return res.status(200).json({ success: true, data: notes });
-  } catch (err) {
-    return res.status(400).json({ success: false, error: err });
-  }
-});
+//CREATE A NOTE
+router.post("/", verify, addNote);
 
-//GET SINGLE NOTE
-router.get("/:id", verify, async (req, res) => {
-  const { id } = req.params;
-  try {
-    const note = await NoteModel.findOne({
-      $and: [{ _id: id }, { userId: req.user.id }],
-    });
-    if (!note) {
-      return res.status(404).json({ success: false, error: "Note Not Found!" });
-    }
-    return res.status(200).json({ success: true, data: note });
-  } catch (err) {
-    return res.status(400).json({ success: false, error: err });
-  }
-});
+//READ ALL NOTES
+router.get("/", verify, getAllNotes);
 
-//ADD A NOTE
-router.post("/", verify, async (req, res) => {
-  const note = {
-    userId: req.user.id,
-    markdown: req.body.markdown,
-  };
-  if (note.markdown === "" || typeof note.markdown !== "string") {
-    return res.status(400).json({
-      success: false,
-      error: "You must provide note object with a string!",
-    });
-  }
-  const newNote = new NoteModel(note);
-  try {
-    await newNote.save();
-    return res.status(200).json({
-      success: true,
-      statusMessage: "New note added succesfully!",
-      data: newNote,
-    });
-  } catch (err) {
-    return res.status(400).json({ error: err });
-  }
-});
-
-//DELETE A NOTE
-router.delete("/:id", verify, async (req, res) => {
-  const { id } = req.params;
-  try {
-    const note = await NoteModel.findOneAndDelete({
-      $and: [{ _id: id }, { userId: req.user.id }],
-    });
-    if (!note) {
-      return res.status(404).json({ success: false, error: "Note Not Found!" });
-    }
-    return res
-      .status(200)
-      .json({ success: true, statusMessage: "Note deleted succesfully!" });
-  } catch (err) {
-    return res.status(400).json({ success: false, error: err });
-  }
-});
+//READ SINGLE NOTE
+router.get("/:id", verify, getSingleNote);
 
 //UPDATE A NOTE
-router.put("/:id", verify, async (req, res) => {
-  const { id } = req.params;
-  const userInput = {
-    markdown: req.body.markdown,
-  };
+router.put("/:id", verify, updateNote);
 
-  if (userInput.markdown === "" || typeof userInput.markdown !== "string") {
-    return res.status(400).json({
-      success: false,
-      error: "You must provide note object with a markdown string!",
-    });
-  }
-  try {
-    const note = await NoteModel.findOne({
-      $and: [{ _id: id }, { userId: req.user.id }],
-    });
-    note.markdown = userInput.markdown;
-    const updatedNote = await note.save({ new: true });
-    if (!updatedNote) {
-      return res.status(404).json({ success: false, error: "Note Not Found!" });
-    }
-    return res.status(200).json({
-      success: true,
-      statusMessage: "Note updated succesfully!",
-      data: updatedNote,
-    });
-  } catch (err) {
-    return res.status(400).json({ success: false, error: err });
-  }
-});
+//DELETE A NOTE
+router.delete("/:id", verify, deleteNote);
 
 module.exports = router;
