8774ab53404db633b014feb18f46b0be5ef40f77
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-26 11:38:30 +0200

    If user is logged in show different navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import { Link, useHistory } from "react-router-dom";
 export default function Navbar() {
 	const history = useHistory();
 	const { user, setUser } = useContext(UserContext);
+
 	const handleLogout = () => {
 		localStorage.removeItem("tkn");
 		setUser(null);
 		history.push("/");
 	};
+	console.log(user);
 
 	return (
 		<ul className="navbar">
@@ -18,12 +20,16 @@ export default function Navbar() {
 					<img className="menu-logo" src="/logo.png" alt="Note Logo" />
 				</Link>
 			</li>
-			<li>
-				<Link to={`/notes/`}>Notes</Link>
-			</li>
-			<li>
-				<p onClick={handleLogout}>Logout</p>
-			</li>
+			{user && (
+				<>
+					<li>
+						<Link to={`/notes/`}>Notes</Link>
+					</li>
+					<li>
+						<p onClick={handleLogout}>Logout</p>
+					</li>
+				</>
+			)}
 		</ul>
 	);
 }
