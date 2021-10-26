3339b56402c46931fd7033638908ba0ec4a6521b
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-26 12:20:27 +0200

    Check if there is an user before showing resource

--- client/src/pages/NotesPage.jsx
+++ client/src/pages/NotesPage.jsx
@@ -1,36 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Note from "../components/Note";
 import CreateNote from "../components/CreateNote";
 import EditNote from "../components/EditNote";
+import { UserContext } from "../context/UserContext";
 
 export default function NotesPage(props) {
 	const [notes, setNotes] = useState(null);
+	const { user } = useContext(UserContext);
+
+	const fetchNotes = async () => {
+		const url = "/api/notes";
+		const token = localStorage.getItem("tkn");
+		const payload = {
+			headers: {
+				"Content-Type": "application/json",
+				authorization: `Bearer ${token}`,
+			},
+		};
+
+		const response = await fetch(url, payload);
+		if (!response.ok) {
+			throw new Error("Something went wrong!");
+		}
+		const responseData = await response.json();
+		setNotes(responseData.data);
+	};
 
 	useEffect(() => {
-		const fetchNotes = async () => {
-			const url = "/api/notes";
-			const token = localStorage.getItem("tkn");
-			const payload = {
-				headers: {
-					"Content-Type": "application/json",
-					authorization: `Bearer ${token}`,
-				},
-			};
-
-			const response = await fetch(url, payload);
-			if (!response.ok) {
-				throw new Error("Something went wrong!");
-			}
-			const responseData = await response.json();
-			setNotes(responseData.data);
-		};
-		fetchNotes().catch((error) => {
-			console.log(error);
-		});
+		if (user) {
+			fetchNotes().catch((error) => {
+				console.log(error);
+			});
+		}
 	}, []);
 
-	if (props.match.params.id && notes) {
+	if (!user) {
+		return (
+			<span>
+				You have to login or sign up to view this page:
+				<Link to="/"> Let's go!</Link>
+			</span>
+		);
+	} else if (props.match.params.id && notes) {
 		const id = props.match.params.id;
 		const note = notes.find((note) => note._id === id);
 		if (note) {
