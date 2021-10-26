29bc592356e978162b225eab1081bc07edac22ab
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-26 11:56:17 +0200

    Refactor handleChange and handleSubmit from signup and login

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,38 +1,6 @@
-import React, { useState, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import { UserContext } from "../context/UserContext";
+import React from "react";
 
-export default function SignUpForm() {
-	const [formFields, setFormFields] = useState(null);
-	const { setUser } = useContext(UserContext);
-	const history = useHistory();
-
-	const handleChange = (value, fieldId) => {
-		const payload = { ...formFields };
-		payload[fieldId] = value;
-		setFormFields(payload);
-	};
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		const url = "/api/users/signup";
-		const payload = {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(formFields),
-		};
-		const response = await fetch(url, payload);
-		if (!response.ok) {
-			throw new Error("Something went wrong!");
-		}
-		const responseData = await response.json();
-		localStorage.setItem("tkn", responseData.token);
-		setUser(responseData.user);
-		history.push(`/notes`);
-	};
-
+export default function SignUpForm({ handleSubmit, handleChange }) {
 	return (
 		<div>
 			<h2>Sign Up</h2>
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,16 +1,47 @@
-import { React, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
 
 export default function LandingPage() {
-	const [formType, setFormType] = useState(null);
+	const [formType, setFormType] = useState("signup");
+	const [formFields, setFormFields] = useState(null);
+	const { setUser } = useContext(UserContext);
+	const history = useHistory();
+
 	const setLogin = () => {
 		setFormType("login");
 	};
 	const setSignUp = () => {
 		setFormType("signup");
+	};
+
+	const handleChange = (value, fieldId) => {
+		const payload = { ...formFields };
+		payload[fieldId] = value;
+		setFormFields(payload);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const url = `/api/users/${formType}`;
+		const payload = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formFields),
+		};
+		const response = await fetch(url, payload);
+		if (!response.ok) {
+			throw new Error("Something went wrong!");
+		}
+		const responseData = await response.json();
+		localStorage.setItem("tkn", responseData.token);
+		setUser(responseData.user);
+		history.push(`/notes`);
 	};
 	return (
 		<div>
@@ -18,7 +49,7 @@ export default function LandingPage() {
 			<br />
 			{formType === "login" ? (
 				<div>
-					<LoginForm />
+					<LoginForm handleSubmit={handleSubmit} handleChange={handleChange} />
 					<br />
 					<Link to="/" onClick={setSignUp}>
 						Not a member? Sign up here!
@@ -26,7 +57,7 @@ export default function LandingPage() {
 				</div>
 			) : (
 				<div>
-					<SignUpForm />
+					<SignUpForm handleSubmit={handleSubmit} handleChange={handleChange} />
 					<br />
 					<Link to="/" onClick={setLogin}>
 						Already have an account? Login here!
