d523d3dab64a89a16f2b79eebc3e438781be415c
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-27 12:11:38 +0200

    Update user model with validation

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const { Schema } = mongoose;
 
@@ -6,19 +7,23 @@ const UserSchema = new Schema(
   {
     displayName: {
       type: String,
-      required: true,
+      required: [true, "A user must have a display name"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "A user must have an email"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
     },
     fullName: {
       type: String,
-      required: true,
+      required: [true, "A user must have a full name"],
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "A user must have a password"],
+      select: false,
     },
   },
   { timestamps: true }
