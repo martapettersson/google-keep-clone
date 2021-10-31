6abb8461521617aabd2813d2477ba8423f9abc40
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-31 18:34:43 +0100

    Add error handling for sign up and login

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import formValidation from "../utils/formValidation";
 
 import LoginForm from "../components/Forms/LoginForm";
 import SignUpForm from "../components/Forms/SignUpForm";
 
 export default function LandingPage() {
 	const [formType, setFormType] = useState("signup");
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(null);
 	const location = useLocation();
-	const [formData, setFormData] = useState(null);
 	const { setUser } = useContext(UserContext);
 	const history = useHistory();
 
@@ -20,24 +22,37 @@ export default function LandingPage() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		const url = `/api/users/${formType}`;
-		const payload = {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(formData),
-		};
-		const response = await fetch(url, payload);
-		if (!response.ok) {
-			throw new Error("Something went wrong!");
+		const validateMessage = formValidation(formData, formType);
+		if (validateMessage === "valid") {
+			const url = `/api/users/${formType}`;
+			const payload = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			};
+
+			try {
+				const response = await fetch(url, payload);
+				const responseData = await response.json();
+
+				if (!response.ok) {
+					setError(responseData.error);
+					throw new Error(responseData.error);
+				}
+				localStorage.setItem("tkn", responseData.token);
+				setUser(responseData.user);
+				history.push(`/notes`);
+			} catch (err) {
+				console.warn(err);
+			}
+		} else {
+			setError(validateMessage);
 		}
-		const responseData = await response.json();
-		localStorage.setItem("tkn", responseData.token);
-		setUser(responseData.user);
-		history.push(`/notes`);
 	};
 	useEffect(() => {
+		setError(null);
 		if (location.pathname === "/login") {
 			setFormType("login");
 		}
@@ -51,12 +66,20 @@ export default function LandingPage() {
 			<br />
 			{formType === "login" ? (
 				<div>
-					<LoginForm handleSubmit={handleSubmit} handleChange={handleChange} />
+					<LoginForm
+						handleSubmit={handleSubmit}
+						handleChange={handleChange}
+						error={error}
+					/>
 					<br />
 				</div>
 			) : (
 				<div>
-					<SignUpForm handleSubmit={handleSubmit} handleChange={handleChange} />
+					<SignUpForm
+						handleSubmit={handleSubmit}
+						handleChange={handleChange}
+						error={error}
+					/>
 					<br />
 				</div>
 			)}
