3d8d284a009e03f97b18979112a8a073319ab73f
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-02 16:53:38 +0100

    Unattach backend validation test from error msg

--- server/__tests__/validation.test.js
+++ server/__tests__/validation.test.js
@@ -6,23 +6,21 @@ test("Verify validatePassword returns true for valid password", () => {
   const password = "AbC45678";
   const passwordConfirm = password;
   const firstCheck = validatePassword(password, passwordConfirm);
-  expect(firstCheck).toBeTruthy();
+  expect(firstCheck.success).toBe(true);
 });
 
 test("Verify validatePassword returns error when password and password confirmation do not match", () => {
   const password = "12345678";
   const passwordConfirm = "01234567";
   const firstCheck = validatePassword(password, passwordConfirm);
-  expect(firstCheck.passwordError).toBe("Passwords do not match.");
+  expect(firstCheck.success).toBe(false);
 });
 
 test("Verify validatePassword returns error for invalid passwords containing special characters", () => {
   const password = '<script>alert("Hello World!")</script>';
   const passwordConfirm = password;
   const firstCheck = validatePassword(password, passwordConfirm);
-  expect(firstCheck.passwordError).toBe(
-    "Password must be at least 8 characters long. No special characters, only letters and numbers."
-  );
+  expect(firstCheck.success).toBe(false);
 });
 
 test("Verify validatePassword returns error for too short passwords", () => {
@@ -31,9 +29,7 @@ test("Verify validatePassword returns error for too short passwords", () => {
   for (let i = 0; i < 7; i++) {
     const passwordConfirm = password;
     const firstCheck = validatePassword(password, passwordConfirm);
-    expect(firstCheck.passwordError).toBe(
-      "Password must be at least 8 characters long. No special characters, only letters and numbers."
-    );
+    expect(firstCheck.success).toBe(false);
     password += character;
   }
 });
