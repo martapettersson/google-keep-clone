c09d0446a9d75fd3193446dd654c293e85e52896
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-02 16:53:23 +0100

    Return success status when validating password

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,14 +1,16 @@
 exports.validatePassword = (password, passwordConfirm) => {
   if (password !== passwordConfirm) {
     return {
+      success: false,
       passwordError: "Passwords do not match.",
     };
   }
   if (!password.match(/^[a-zA-Z0-9]{8}$/)) {
     return {
+      success: false,
       passwordError:
         "Password must be at least 8 characters long. No special characters, only letters and numbers.",
     };
   }
-  return true;
+  return { success: true };
 };
